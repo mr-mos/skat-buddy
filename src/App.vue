826dbe71808de727c1e64fff5de165bf43ce72d5
mos <mos.cheele+github@gmail.com>
2021-10-06 17:07:54 +0200

    Show all cards responsive in a Card-Component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
+
   <div id="screen">
-    <h1>Skat Buddy</h1>
+
+    <div v-for="(color,colorIndex) in cardColors" style="display: flex;"
+         :key="colorIndex">
+        <div v-for="(type,typeIndex) in cardTypes" style="flex:1; margin:5px"
+           :key="typeIndex">
+          <Card :name="type+'_'+color"/>
+        </div>
+    </div>
+
   </div>
+
 </template>
 
+<!-- -------------------------------------------------------------   -->
+
 <script>
+
+import Card from "@/components/Card";
 
 export default {
   name: 'App',
   components: {
+    Card
+  },
+  data() {
+    return {
+      cardColors: ['club', 'spade', 'heart', 'diamond'],
+      cardTypes: ['7', '8', '9', '10', 'jack', 'queen', 'king', 'ace']
+    }
   }
 }
 </script>
+
+<!-- -------------------------------------------------------------   -->
 
 <style scoped>
 
---
+++ src/components/Card.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="playingCard">
+      <img :src="imagePath"/>
+  </div>
+</template>
+
+<!-- -------------------------------------------------------------   -->
+
+<script>
+
+export default {
+  name: 'Card',
+  props: ['name'],
+  methods: {},
+  computed: {
+    imagePath() {
+      return require("@/assets/cards/"+this.name+".svg")
+    }
+  }
+}
+
+</script>
+
+<!-- -------------------------------------------------------------   -->
+
+<style scoped>
+
+.playingCard {
+  max-width: 300px;
+  margin-left:auto;
+  margin-right: auto;
+}
+
+.playingCard img {
+  width: 100%;
+}
+
+</style>
